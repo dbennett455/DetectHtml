4f6081bcc06858f0c2b3a3891c0508607b3e27f3
pallavJha <pallav_kumar71@yahoo.com>
2017-05-15 18:43:50 +0530

    Added code for DetectHTML.java, sonar changes and JavaDoc

--- src/org/github/DetectHtml.java
+++ src/org/github/DetectHtml.java
@@ -5,39 +5,63 @@ package org.github;
  * This will detect tags or entities
  *
  * @author dbennett455@gmail.com - David H. Bennett
- *
  */
 
 import java.util.regex.Pattern;
 
-public class DetectHtml
-{
-	// adapted from post by Phil Haack and modified to match better
-	public final static String tagStart=
-		"\\<\\w+((\\s+\\w+(\\s*\\=\\s*(?:\".*?\"|'.*?'|[^'\"\\>\\s]+))?)+\\s*|\\s*)\\>";
-	public final static String tagEnd=
-		"\\</\\w+\\>";
-	public final static String tagSelfClosing=
-		"\\<\\w+((\\s+\\w+(\\s*\\=\\s*(?:\".*?\"|'.*?'|[^'\"\\>\\s]+))?)+\\s*|\\s*)/\\>";
-	public final static String htmlEntity=
-		"&[a-zA-Z][a-zA-Z0-9]+;";
-	public final static Pattern htmlPattern=Pattern.compile(
-	  "("+tagStart+".*"+tagEnd+")|("+tagSelfClosing+")|("+htmlEntity+")",
-	  Pattern.DOTALL
-	);
+public class DetectHtml {
 
-	/**
-	 * Will return true if s contains HTML markup tags or entities.
-	 *
-	 * @param s String to test
-	 * @return true if string contains HTML
-	 */
-	public static boolean isHtml(String s) {
-		boolean ret=false;
-		if (s != null) {
-			ret=htmlPattern.matcher(s).find();
-		}
-		return ret;
-	}
+    /**
+     * Matches:
+     * <code>
+     * <td valign="top" align="center" width="598" height="46">
+     * </code>
+     */
+    // adapted from post by Phil Haack and modified to match better
+    private static final String TAG_START =
+            "\\<\\w+((\\s+\\w+(\\s*\\=\\s*(?:\".*?\"|'.*?'|[^'\"\\>\\s]+))?)+\\s*|\\s*)\\>";
+
+    /**
+     * Matches:
+     * <code>
+     * </td>
+     * </code>
+     */
+    private static final String TAG_END =
+            "\\</\\w+\\>";
+
+
+    /**
+     * Matches:
+     * <code>
+     * <img style="padding-top:112px" height="92" src="/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" />
+     * </code>
+     */
+    private static final String TAG_SELF_CLOSING =
+            "\\<\\w+((\\s+\\w+(\\s*\\=\\s*(?:\".*?\"|'.*?'|[^'\"\\>\\s]+))?)+\\s*|\\s*)/\\>";
+    private static final String HTML_ENTITY =
+            "&[a-zA-Z][a-zA-Z0-9]+;";
+    private static final Pattern htmlPattern = Pattern.compile(
+            "(" + TAG_START + ".*" + TAG_END + ")|(" + TAG_SELF_CLOSING + ")|(" + HTML_ENTITY + ")",
+            Pattern.DOTALL
+    );
+
+    private DetectHtml() {
+
+    }
+
+    /**
+     * Will return true if s contains HTML markup tags or entities.
+     *
+     * @param s String to test
+     * @return true if string contains HTML
+     */
+    public static boolean isHtml(String s) {
+        boolean ret = false;
+        if (s != null) {
+            ret = htmlPattern.matcher(s).find();
+        }
+        return ret;
+    }
 
 }
